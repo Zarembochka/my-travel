574ae8a8a0fc3d27152a3e23b7ef3d3b2d72afb3
Luba Hadunova <luba.godunova2018@gmail.com>
2023-07-04 14:25:07 +0200

    feat: add desktop & mobile version

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 //Burger handler
 (function () {
     const burgerItem = document.querySelector('.burger__menu');
-    console.log(burgerItem);
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav__close');
+    
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav__active');
-        console.log(burgerItem);
     });
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav__active');
     });
-    console.log(burgerItem);
+
+    document.addEventListener('click', (event) => {
+        let target = event.target.closest('.header__nav');
+
+        if (!target 
+            && !event.target.classList.contains('burger__menu__line')) {
+                menu.classList.remove('header__nav__active');
+        }
+    });
 }())
